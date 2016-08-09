92fb9cd633357344043c4cff1cccde143e5f309f
Joseph C Wang <joequant@gmail.com>
2016-08-09 21:32:06 +0800

    add location

--- test_loader.py
+++ test_loader.py
@@ -1,9 +1,12 @@
 import cffi
+import os
 import ctypes
+location = os.path.dirname(os.path.realpath(__file__))
+dll_location = os.path.join(location, "dll")
 print ("Testing loader")
-ctypes.windll.LoadLibrary("dll\\libeay32.dll")
-ctypes.windll.LoadLibrary("dll\\ssleay32.dll")
-ctypes.windll.LoadLibrary("dll\\spapidllm32.dll")
+ctypes.windll.LoadLibrary(os.path.join(dll_location, "libeay32.dll"))
+ctypes.windll.LoadLibrary(os.path.join(dll_location, "ssleay32.dll"))
+ctypes.windll.LoadLibrary(os.path.join(dll_location, "spapidllm32.dll"))
 print(ctypes.windll.spapidllm32)
 
 input("Press any key to exit")
