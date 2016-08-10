b50c0fcd312abdb0f8c48db1abaa3ddd70d97b8b
Joseph C Wang <joequant@gmail.com>
2016-08-10 13:15:27 +0800

    run initialize

--- test_loader.py
+++ test_loader.py
@@ -4,10 +4,14 @@ import ctypes
 location = os.path.dirname(os.path.realpath(__file__))
 dll_location = os.path.join(location, "dll")
 print ("Testing loader")
+print ("Loading dlls from directory '%s'" % dll_location)
 ctypes.windll.LoadLibrary(os.path.join(dll_location, "libeay32.dll"))
 ctypes.windll.LoadLibrary(os.path.join(dll_location, "ssleay32.dll"))
 ctypes.windll.LoadLibrary(os.path.join(dll_location, "spapidllm32.dll"))
 print(ctypes.windll.spapidllm32)
+
+sptrader = ctypes.windll.spapidllm32;
+print(sptrader.SPAPI_Initialize())
 
 input("Press any key to exit")
 
