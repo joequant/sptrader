facc6281b524b6102bce6b25a2704514cc7748fa
Joseph C Wang <joequant@gmail.com>
2016-08-11 10:17:10 +0800

    move to test loader

--- sptrader.py
+++ sptrader.py
@@ -8,7 +8,12 @@ ctypes.windll.LoadLibrary(os.path.join(dll_location, "libeay32.dll"))
 ctypes.windll.LoadLibrary(os.path.join(dll_location, "ssleay32.dll"))
 ctypes.windll.LoadLibrary(os.path.join(dll_location, "spapidllm32.dll"))
 sp = ctypes.windll.spapidllm32
+sp.SPAPI_SetLanguageId(0)
 sp.SPAPI_Initialize()
+
+# Remember to convert unicode strings to byte strings otherwise
+# ctypes will assume that the characters are wchars and not
+# ordinary characters
 
 class SPTrader(object):
     def __init__(self,
@@ -18,17 +23,17 @@ class SPTrader(object):
                  app_id,
                  user_id,
                  password):
-        self.user = user_id
-        sp.SPAPI_SetLoginInfo(host,
+        self.user = user_id.encode("utf-8")
+        sp.SPAPI_SetLoginInfo(host.encode("utf-8"),
                               port,
-                              license,
-                              app_id,
-                              user_id,
-                              password)
+                              license.encode("utf-8"),
+                              app_id.encode("utf-8"),
+                              user_id.encode("utf-8"),
+                              password.encode("utf-8"))
     def login(self):
         return sp.SPAPI_Login()
-    def get_login_status(self, host_id):
-        return sp.SPAPI_GetLoginStatus(self.user, host_id)
+    def get_login_status(self, status_id):
+        return sp.SPAPI_GetLoginStatus(self.user, status_id)
     def logout(self):
         return sp.SPAPI_Logout(self.user)
     
--- test_loader.py
+++ test_loader.py
@@ -1,6 +1,7 @@
 import cffi
 import os
 import sys
+import time
 
 location = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, location)
@@ -8,9 +9,10 @@ import sptrader
 sptrader = sptrader.SPTrader("demo.spsystem.info", 8080,
                              "DLLAPITEST",
                              "DLLAPITEST",
-                             "SPAPI47",
+                             "SPAPI02",
                              "12345678")
 print(sptrader.login())
+time.sleep(5)
 print(sptrader.get_login_status(81))
 print(sptrader.logout())
 input("Press any key to exit")
