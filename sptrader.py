6829dd81bb7735767e949fca9c269d6164f72e79
Joseph C Wang <joequant@gmail.com>
2016-08-13 03:36:00 +0800

    change from ctypes to cffi

--- sptrader.py
+++ sptrader.py
@@ -1,22 +1,37 @@
-import cffi
-from ctypes import *
-import ctypes
+from cffi import FFI
 import atexit
 import os
 location = os.path.dirname(os.path.realpath(__file__))
 dll_location = os.path.join(location, "dll")
-ctypes.windll.LoadLibrary(os.path.join(dll_location, "libeay32.dll"))
-ctypes.windll.LoadLibrary(os.path.join(dll_location, "ssleay32.dll"))
-ctypes.windll.LoadLibrary(os.path.join(dll_location, "spapidllm32.dll"))
-sp = ctypes.windll.spapidllm32
+ffi = FFI()
+ffi.cdef("""
+typedef void (__stdcall *LoginReplyAddr)(long ret_code, char* ret_msg);
+typedef void (__stdcall *AccountLoginReplyAddr)(char *accNo, long ret_code, char* ret_msg);
+typedef void (__stdcall *AccountLogoutReplyAddr)(long ret_code, char *ret_msg);
+
+int SPAPI_SetLanguageId(int langid);
+int SPAPI_Initialize();
+void SPAPI_SetLoginInfo(char *host, int port, char *license, char *app_id, char *user_id, char *password);
+int SPAPI_Login();
+int SPAPI_GetLoginStatus(char *user_id, short host_id);
+void SPAPI_RegisterLoginReply(LoginReplyAddr addr);
+int SPAPI_Logout(char *user_id);
+void SPAPI_RegisterAccountLoginReply(AccountLoginReplyAddr addr);
+void SPAPI_RegisterAccountLogoutReply(AccountLogoutReplyAddr addr);
+void SPAPI_Uninitialize();
+""")
+ffi.dlopen(os.path.join(dll_location, "libeay32.dll"))
+ffi.dlopen(os.path.join(dll_location, "ssleay32.dll"))
+sp = ffi.dlopen(os.path.join(dll_location, "spapidllm32.dll"))
 sp.SPAPI_SetLanguageId(0)
 sp.SPAPI_Initialize()
-LOGIN_FUNC = WINFUNCTYPE(None, c_long, c_char_p)
+
 # Remember to convert unicode strings to byte strings otherwise
 # ctypes will assume that the characters are wchars and not
 # ordinary characters
 
 class SPTrader(object):
+    ffi = ffi
     def __init__(self,
                  host,
                  port,
--- test_loader.py
+++ test_loader.py
@@ -2,11 +2,13 @@ import cffi
 import os
 import sys
 import time
+import threading
 from ctypes import *
 
 location = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, location)
 import sptrader
+cv = threading.Condition()
 sptrader = sptrader.SPTrader("demo.spsystem.info", 8080,
                              "57ABC58F6555F",
                              "SPDEMO",
@@ -14,13 +16,17 @@ sptrader = sptrader.SPTrader("demo.spsystem.info", 8080,
                              "test1")
 
 
-LOGIN_FUNC = WINFUNCTYPE(None, c_long, c_char_p)
+@sptrader.ffi.callback("LoginReplyAddr")
 def login_actions(ret_code, ret_msg):
+    cv.acquire()
     print("login")
     print(sptrader.get_login_status(81))
     print(sptrader.logout())
-login_func = LOGIN_FUNC(login_actions)
-    
-print(sptrader.login(login_func))
+    input("Press any key to exit")
+    cv.notify()
+    cv.release()
 
-input("Press any key to exit")
+cv.acquire()
+print(sptrader.login(login_actions))
+cv.wait()
+cv.release()
