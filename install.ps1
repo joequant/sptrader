dc2ed843f290ab012a077f3b99650ae294bef944
Joseph C Wang <joequant@gmail.com>
2016-08-14 09:22:50 +0800

    add nodejs

--- install.ps1
+++ install.ps1
@@ -8,22 +8,28 @@ $pypath = "c:\\Program Files (x86)\\Python35-32\\Scripts"
 $vcredist = "vcredist_x86.exe"
 $spzipfile = "SPAPIDLL_R8.742_WIN32.zip"
 $sslzipfile = "openssl-1.0.2h-i386-win32.zip"
+$nodeversion = "v4.4.7"
+$nodefile = "node-$($nodeversion)-x86.msi"
 
 #start
 hello_world
 Write-Output "Starting install"
-Write-Output "Downloading python"
+Write-Output "Installing python"
 wget "https://www.python.org/ftp/python/$($pyversion)/$($pyfile)" -OutFile "$($env:TEMP)/$($pyfile)"
 Start-Process "$($env:TEMP)/$($pyfile)" "PrependPath=1 InstallAllUsers=1 Include_test=0" -Wait
 Remove-Item "$($env:TEMP)/$($pyfile)"
 $env:Path += ";$($pypath)"
-Start-Process pip "install cffi Flask gevent" -Verb runAs
+Start-Process pip "install cffi Flask gevent" -Verb runAs -Wait
 
-Write-Output "Downloading VC redistribution"
-wget "http://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/$($vcredist)" -OutFile "$($env:TEMP)/$($vcredist)"
 Write-Output "Installing VC redistribution"
+wget "http://download.microsoft.com/download/1/6/5/165255E7-1014-4D0A-B094-B6A430A6BFFC/$($vcredist)" -OutFile "$($env:TEMP)/$($vcredist)"
 Start-Process "$($env:TEMP)/$($vcredist)" -Wait
 Remove-Item "$($env:TEMP)/$($vcredist)"
+
+Write-Output "Installing node"
+wget "https://nodejs.org/dist/$($nodeversion)/$($nodefile)" -OutFile "$($env:TEMP)/$($nodefile)"
+Start-Process "$($env:TEMP)/$($nodefile)" -Wait
+Remove-Item "$($env:TEMP)/$($nodefile)"
 
 New-Item dll -type directory -force
 unzip -filename $spzipfile -url http://spsystem.info/download/API/R8742 -dir dll -name SPTrader
