5ee199c8f07efa79dfb9e0904473f95059342d06
Joseph C Wang <joequant@gmail.com>
2016-08-17 22:26:52 +0800

    fix loader

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -13,25 +13,26 @@ import sptrader
 import config
 cv = threading.Condition()
 login = config.logininfo;
-sptrader = sptrader.SPTrader(login['host'],
-                             login['port'],
-                             login['license'],
-                             login['app_id'],
-                             login['user_id'],
-                             "test1")
+sp = sptrader.SPTrader()
+sp.set_login_info(login['host'],
+                  login['port'],
+                  login['license'],
+                  login['app_id'],
+                  login['user_id'],
+                  "test1")
 
 
-@sptrader.ffi.callback("LoginReplyAddr")
+@sp.ffi.callback("LoginReplyAddr")
 def login_actions(ret_code, ret_msg):
     cv.acquire()
     print("login")
-    print(sptrader.get_login_status(81))
-    print(sptrader.logout())
+    print(sp.get_login_status(81))
+    print(sp.logout())
     input("Press any key to exit")
     cv.notify()
     cv.release()
 
 cv.acquire()
-print(sptrader.login(login_actions))
+print(sp.login(login_actions))
 cv.wait()
 cv.release()
