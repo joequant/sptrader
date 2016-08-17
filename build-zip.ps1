61b36d5a3ec61ad3d14d859a583f4f3c4de35916
Joseph C Wang <joequant@gmail.com>
2016-08-17 10:13:05 +0800

    add build-zip.ps1

---
+++ build-zip.ps1
@@ -0,0 +1,6 @@
+cd ..
+Compress-Archive -Path sptrader\*.md -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\*.ps1 -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\sptrader -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\static -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\tests -Update -DestinationPath sptrader.zip
