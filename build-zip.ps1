2b565a9224fc3463a9ef1c54ccfce0b7d7e5d27d
Joseph C Wang <joequant@gmail.com>
2016-08-19 04:49:30 +0800

    fix build zip

--- build-zip.ps1
+++ build-zip.ps1
@@ -1,7 +1,12 @@
 cd ..
+If (Test-Path sptrader.zip) {
+   Remove-Item sptrader.zip
+}
 Compress-Archive -Path sptrader\*.md -DestinationPath sptrader.zip
 Compress-Archive -Path sptrader\*.ps1 -Update -DestinationPath sptrader.zip
 Compress-Archive -Path sptrader\*.psm -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\sptrader -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\static -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\tests -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\sptrader\*.py -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\static\*.html -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\static\*.css -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\static\*.js -Update -DestinationPath sptrader.zip
+Compress-Archive -Path sptrader\tests\*.py -Update -DestinationPath sptrader.zip
