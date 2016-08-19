a222228bdf5ddc69e2afa5e3c8827b5db25e4cf7
Joseph C Wang <joequant@gmail.com>
2016-08-19 18:28:20 +0800

    fix build-zip

--- build-zip.ps1
+++ build-zip.ps1
@@ -1,12 +1,21 @@
 cd ..
+Remove-Item build -Force -Recurse
+New-Item -ItemType directory build -Force
+cd build
 If (Test-Path sptrader.zip) {
    Remove-Item sptrader.zip
 }
-Compress-Archive -Path sptrader\*.md -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\*.ps1 -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\*.psm -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\sptrader\*.py -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\static\*.html -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\static\*.css -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\static\*.js -Update -DestinationPath sptrader.zip
-Compress-Archive -Path sptrader\tests\*.py -Update -DestinationPath sptrader.zip
+New-Item -ItemType directory sptrader
+New-Item -ItemType directory sptrader\sptrader
+New-Item -ItemType directory sptrader\static
+New-Item -ItemType directory sptrader\tests
+Copy-Item ..\sptrader\*.md sptrader
+Copy-Item ..\sptrader\*.ps1 sptrader
+Copy-Item ..\sptrader\*.psm sptrader
+Copy-Item ..\sptrader\*.json sptrader
+Copy-Item ..\sptrader\sptrader\*.py sptrader\sptrader
+Copy-Item ..\sptrader\static\*.html sptrader\static
+Copy-Item ..\sptrader\static\*.css sptrader\static
+Copy-Item ..\sptrader\static\*.js sptrader\static
+Copy-Item ..\sptrader\tests\*.py sptrader\tests
+Compress-Archive -Path sptrader -DestinationPath sptrader.zip
