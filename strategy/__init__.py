9693df0eacfc24721bca21d8550bb27112b41052
Joseph C Wang <joequant@gmail.com>
2016-09-23 09:34:43 +0800

    refactor strategy

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -1,18 +1,82 @@
+import matplotlib
+matplotlib.use('Agg', warn=False, force=True)
+import backtrader as bt
+import matplotlib.pyplot as plt
+from backtrader.plot.plot import Plot
+
 import strategy.sample
 import sys
 import os
+import multiprocessing
+from spfeed import SharpPointCSVData
+from spbroker import SharpPointBroker
+import spstore
+
+class Unbuffered(object):
+    def __init__(self, stream):
+        self.stream = stream
+    def write(self, data):
+        self.stream.write(data)
+        self.stream.flush()
+    def __getattr__(self, attr):
+        return getattr(self.stream, attr)
+
+def run_strategy(module, fname, kwargs):
+    modpath = os.path.dirname(os.path.realpath(__file__))
+    logpath = os.path.join(modpath, '../data/log-%s-%s.txt' % \
+                           (kwargs['strategy'],
+                            str(kwargs['id'])))
+    f = open(logpath, "w")
+    sys.stdout = Unbuffered(f)
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(module)
+    store = spstore.SharpPointStore()
+    broker = store.getbroker(backtest=kwargs.get('backtest', False))
+    cerebro.setbroker(broker)
+
+    # Create a Data Feed
+    data = store.getdata(
+        dataname=fname,
+        **kwargs)
+    data2 = bt.DataClone(dataname=data)
+    data2.addfilter(bt.ReplayerMinutes, compression=5)
+    cerebro.adddata(data)
+    cerebro.adddata(data2)
+
+    # Set our desired cash start
+    cerebro.broker.setcash(100000.0)
+
+    # Set the commission - 0.1% ... divide by 100 to remove the %
+    cerebro.broker.setcommission(commission=0.001)
+
+    # Print out the starting conditions
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # Run over everything
+    cerebro.run()
+    plotter = Plot(style='candle')
+    cerebro.plot(plotter)
+    imgdata = open('out.svg', 'wb')
+    plt.savefig(imgdata, format='svg')
+    imgdata.close()
+    # Print out the final result
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    f.close()
+    return None
+
 
 def run(name, id, kwargs):
     if name == "sample":
+        module = strategy.sample.TestStrategy
         modpath = os.path.dirname(os.path.realpath(__file__))
-        logpath = os.path.join(modpath, '../data/log-%s-%s.txt' % \
-                               (name, str(id)))
-        sys.stdout = open(logpath, "w")
+        datapath = os.path.join(modpath, '../data/ticker.txt')
         kwargs['newdata'] = True
         kwargs['keepalive'] = True
         kwargs['debug'] = True
         kwargs['streaming'] = True
-        return sample.run(kwargs)
+        p = multiprocessing.Process(target=run_strategy,
+                                    args=(module, datapath, kwargs))
+        return p
 
 if __name__ == '__main__':
     run("sample", 1)
--- strategy/sample.py
+++ strategy/sample.py
@@ -7,15 +7,11 @@ from backtrader.plot.plot import Plot
 import datetime  # For datetime objects
 import os.path  # To manage paths
 import sys  # To find out the script name (in argv[0])
-import multiprocessing
 # Create a Strategy
 
 location = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, location)
 sys.path.insert(0, os.path.join(location, "../sptrader"))
-from spfeed import SharpPointCSVData
-from spbroker import SharpPointBroker
-import spstore
 
 class TestStrategy(bt.Strategy):
     params = (
@@ -108,77 +104,3 @@ class TestStrategy(bt.Strategy):
 
                 # Keep track of the created order to avoid a 2nd order
                 self.order = self.sell()
-
-
-class Unbuffered(object):
-    def __init__(self, stream):
-        self.stream = stream
-    def write(self, data):
-        self.stream.write(data)
-        self.stream.flush()
-    def __getattr__(self, attr):
-        return getattr(self.stream, attr)
-
-def run_strategy(fname, kwargs):
-    modpath = os.path.dirname(os.path.realpath(__file__))
-    logpath = os.path.join(modpath, '../data/log-%s-%s.txt' % \
-                           (kwargs['strategy'],
-                            str(kwargs['id'])))
-    f = open(logpath, "w")
-    sys.stdout = Unbuffered(f)
-    cerebro = bt.Cerebro()
-    cerebro.addstrategy(TestStrategy)
-    store = spstore.SharpPointStore()
-    broker = store.getbroker(backtest=kwargs.get('backtest', False))
-    cerebro.setbroker(broker)
-
-    # Create a Data Feed
-    data = store.getdata(
-        dataname=fname,
-        **kwargs)
-    data2 = bt.DataClone(dataname=data)
-    data2.addfilter(bt.ReplayerMinutes, compression=5)
-    cerebro.adddata(data)
-    cerebro.adddata(data2)
-
-    # Set our desired cash start
-    cerebro.broker.setcash(100000.0)
-
-    # Set the commission - 0.1% ... divide by 100 to remove the %
-    cerebro.broker.setcommission(commission=0.001)
-
-    # Print out the starting conditions
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-    # Run over everything
-    cerebro.run()
-    plotter = Plot(style='candle')
-    cerebro.plot(plotter)
-    imgdata = open('out.svg', 'wb')
-    plt.savefig(imgdata, format='svg')
-    imgdata.close()
-    # Print out the final result
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    f.close()
-    return None
-
-def run(args):
-    # Datas are in a subfolder of the samples. Need to find where the script is
-    # because it could have been called from anywhere
-    modpath = os.path.dirname(os.path.realpath(__file__))
-    datapath = os.path.join(modpath, '../data/ticker.txt')
-    p = multiprocessing.Process(target=run_strategy, args=(datapath, args))
-    return p
-
-if __name__ == '__main__':
-    p = run({
-        "strategy" : "sample",
-        "id" : 1,
-        "product" : "HSIZ6",
-        "newdata": True,
-        "keepalive": True,
-        "debug" : True,
-        'backtest' : False})
-    p.start()
-    p.join()
-
