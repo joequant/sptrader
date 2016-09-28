d4facea43f05765bc4c60642995c185e8f901b76
Joseph C Wang <joequant@gmail.com>
2016-09-29 05:05:14 +0800

    add backtest

---
+++ tests/test_backtest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import os
+import sys
+location = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, location)
+sys.path.insert(0, os.path.join(location, ".."))
+sys.path.insert(0, os.path.join(location, "..", "sptrader"))
+
+import strategy
+print(strategy.backtest({"dataname": "HSIZ6",
+                         "strategy" : "sample"}))
