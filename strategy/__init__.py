e938281f248088e6d14b73628b3f41b14ec69379
Joseph C Wang <joequant@gmail.com>
2016-09-29 05:22:10 +0800

    refactor and pep8-ize

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -1,12 +1,12 @@
+import sys
+import os
+import io
+import base64
 import matplotlib
 matplotlib.use('Agg', warn=False, force=True)
 import backtrader as bt
 import matplotlib.pyplot as plt
 from backtrader.plot.plot import Plot
-import sys
-import os
-import io
-import base64
 from multiprocessing import Process, Queue
 from spfeed import SharpPointCSVData
 from spbroker import SharpPointBroker
@@ -15,14 +15,18 @@ import strategy.strategylist
 from datetime import datetime
 from pytz import timezone
 
+
 class Unbuffered(object):
     def __init__(self, stream):
         self.stream = stream
+
     def write(self, data):
         self.stream.write(data)
         self.stream.flush()
+
     def __getattr__(self, attr):
         return getattr(self.stream, attr)
+
 
 def run_strategy(name, kwargs, q):
     try:
@@ -31,7 +35,7 @@ def run_strategy(name, kwargs, q):
             raise ValueError('missing dataname')
         module = strategylist.dispatch[name]
         modpath = os.path.dirname(os.path.realpath(__file__))
-        logpath = os.path.join(modpath, '../data/log-%s-%s.txt' % \
+        logpath = os.path.join(modpath, '../data/log-%s-%s.txt' %
                                (kwargs['strategy'],
                                 str(kwargs['id'])))
         f = open(logpath, "w")
@@ -41,7 +45,7 @@ def run_strategy(name, kwargs, q):
         store = spstore.SharpPointStore()
         broker = store.getbroker(backtest=kwargs.get('backtest', False))
         cerebro.setbroker(broker)
-        
+
         # Create a Data Feed
         data = store.getdata(
             **kwargs)
@@ -52,7 +56,7 @@ def run_strategy(name, kwargs, q):
 
         # Print out the starting conditions
         print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    
+
         # Run over everything
         cerebro.run()
         # Print out the final result
@@ -64,6 +68,7 @@ def run_strategy(name, kwargs, q):
         q.put((kwargs['strategy'], kwargs['id'], "error",
                repr(sys.exc_info())))
         raise
+
 
 def run_backtest(kwargs):
     if kwargs.get('dataname', None) is None or \
@@ -79,7 +84,7 @@ def run_backtest(kwargs):
     store = spstore.SharpPointStore()
     broker = store.getbroker(backtest=kwargs.get('backtest', True))
     cerebro.setbroker(broker)
-    
+
     # Create a Data Feed
     data = store.getdata(
         **kwargs)
@@ -87,20 +92,20 @@ def run_backtest(kwargs):
     data2.addfilter(bt.ReplayerMinutes, compression=5)
     cerebro.adddata(data)
     cerebro.adddata(data2)
-    
+
     # Set the commission - 0.1% ... divide by 100 to remove the %
     cerebro.broker.setcommission(commission=0.0)
-    
+
     # Print out the starting conditions
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    
+
     # Run over everything
     cerebro.run()
     plotter = Plot(style='candle')
     cerebro.plot(plotter)
-    imgdata = io.BytesIO() 
+    imgdata = io.BytesIO()
     plt.savefig(imgdata, format='svg')
-    
+
     # Print out the final result
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
     retval = '<img src="data:image/svg+xml;base64,%s" /><br>' % \
@@ -110,6 +115,7 @@ def run_backtest(kwargs):
     f.close()
     sys.stdout = old_stdout
     return retval
+
 
 def run(name, id, kwargs):
     q = Queue()
@@ -127,6 +133,7 @@ def run(name, id, kwargs):
     p.start()
     return (p, q)
 
+
 def backtest(kwargs):
     modpath = os.path.dirname(os.path.realpath(__file__))
     datapath = os.path.join(modpath, '../data/ticker.txt')
@@ -138,42 +145,50 @@ def backtest(kwargs):
     kwargs['streaming'] = False
     return run_backtest(kwargs)
 
+
 def strategy_list():
     return list(strategy.strategylist.dispatch.keys())
 
+
 def params(name):
     return strategy.strategylist.dispatch[name].params._getpairs()
+
 
 def headers(name):
     headers = strategy.strategylist.dispatch[name].headers()
     defaultData = params(name)
     for i in range(len(headers)):
-        if not 'defaultData' in headers[i] and \
+        if 'defaultData' not in headers[i] and \
                headers[i]['field'] in defaultData:
             headers[i]['defaultData'] = defaultData[headers[i]['field']]
     return headers
 
 
-def string_to_seconds(s):
-    v = s.split(":")
-    r = int(v[0]) * 3600 + int(v[1]) * 60
-    if len(v) > 2:
-        r = r + int(v[2])
-    return r
+class TimeFilter(object):
+    def __init__(self, a, b):
+        self.start_time = self.string_to_seconds(a)
+        self.end_time = self.string_to_seconds(b)
 
-def seconds_tz(tz='UTC'):
-    timez = timezone(tz)
-    now = datetime.now(timez)
-    return (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+    @staticmethod
+    def string_to_seconds(s):
+        v = s.split(":")
+        r = int(v[0]) * 3600 + int(v[1]) * 60
+        if len(v) > 2:
+            r = r + int(v[2])
+        return r
 
-def seconds_from_midnight(s):
-    return (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+    @staticmethod
+    def seconds_from_midnight(d):
+        return (d - d.replace(hour=0, minute=0,
+                              second=0, microsecond=0)).total_seconds()
+
+    def filter(self, a):
+        ticktime_seconds = self.seconds_from_midnight(a)
+        return (ticktime_seconds >= self.start_time and
+                ticktime_seconds <= self.end_time)
 
 if __name__ == '__main__':
     print(params('sample'))
     print(params('sample').get('exitbars', None))
-    print(string_to_seconds("14:30"))
-    print(seconds_tz())
-    print(seconds_tz('Asia/Hong_Kong'))
-    run("sample", 1, {'exitbars':1})
-
+    print(TimeFilter.string_to_seconds("14:30"))
+    run("sample", 1, {'exitbars': 1})
