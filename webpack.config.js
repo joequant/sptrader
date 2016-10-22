c9859bd295d910fbf5799b6aad5e2654bd906668
Joseph C Wang <joequant@gmail.com>
2016-10-22 22:23:55 +0800

    add stage-0

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 		  retainLines: true,
 		  cacheDirectory: true,
 		  plugins: ['transform-runtime'],
-		  presets: ['es2015', 'react', 'stage-2']
+		  presets: ['es2015', 'react', 'stage-0']
 	      }
 	    }
 	]
