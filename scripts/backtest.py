510d68c1f340ae267c3094ad832284588f21a71e
Joseph C Wang <joequant@gmail.com>
2016-11-29 19:42:23 +0800

    backtest

--- scripts/backtest.py
+++ scripts/backtest.py
@@ -4,9 +4,17 @@ import json
 import sys
 from pprint import pprint
 import requests
+import re
+import os
 
-with open(sys.argv[1]) as data_file:
-        data = json.load(data_file)
-        r = requests.post("http://localhost:5000/backtest",
-                          data)
-        print(r.text)
+for i in sys.argv[1:]:
+	if re.search('\.json$', i):
+		with open(i) as data_file:
+			data = json.load(data_file)
+			r = requests.post("http://localhost:5000/backtest",
+					  data)
+			outfile = re.sub('\.json$', '.html',
+					 os.path.basename(i))
+			print("writing to ", outfile)
+			with open(outfile, 'w') as outf:
+				print(r.text, file=outf)
