031028e6e64119f337a746fd51a0af7f6da84c78
Joseph C Wang <joequant@gmail.com>
2016-12-14 14:05:56 +0800

    add items

---
+++ admin/install-mageia.sh
@@ -0,0 +1,3 @@
+sudo urpmi python3-cffi python3-flask python3-matplotlib \
+     python3-requests python3-pytz python3-pip
+sudo pip3 install backtrader==1.9.12.99 sseclient-py
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='sptrader',
+      version='0.5.1',
+      description='SharpPoint trading system',
+      author='Joseph C Wang',
+      author_email='joequant@gmail.com',
+      url='https://github.com/joequant/sptrader',
+      packages=['sptrader'],
+      install_requires=[
+    'cffi',
+    'flask',
+    'backtrader==1.9.12.99',
+    'matplotlib',
+    'sseclient-py',
+    'requests',
+    'pytz'
+    ]
+      )
