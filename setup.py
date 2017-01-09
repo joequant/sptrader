5aea8d2c85d2d2b8b29da5a2d7727e1448902929
Joseph C Wang <joequant@gmail.com>
2017-01-10 07:04:37 +0800

    v0.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='sptrader',
-      version='0.7.0',
+      version='0.8.0',
       description='SharpPoint trading system',
       author='Joseph C Wang',
       author_email='joequant@gmail.com',
