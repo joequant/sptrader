66ba36ce790a4fe61950d1a78f1d20586a831f9c
Joseph C Wang <joequant@gmail.com>
2017-04-17 18:58:13 +0800

    add tendo

--- admin/install-mageia.sh
+++ admin/install-mageia.sh
@@ -1,3 +1,4 @@
 sudo urpmi python3-cffi python3-flask python3-matplotlib \
      python3-requests python3-pytz python3-pip
-sudo pip3 install backtrader==1.9.12.99 sseclient-py
+sudo pip3 install backtrader==1.9.12.99 sseclient-py tendo
+
