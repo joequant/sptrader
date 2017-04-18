73d6cd3471f3df20c814b447d8fd1c887d5ee3c6
Joseph C Wang <joequant@gmail.com>
2017-04-19 00:00:36 +0800

    add spwriter

--- sptrader/spwriter.py
+++ sptrader/spwriter.py
@@ -1,5 +1,24 @@
 import backtrader as bt
+import itertools
 
 class SharpPointWriter(bt.WriterFile):
+    params = (
+        ('csvsep', '; '),
+        )
     def __init__(self, *args, **kwargs):
         super(SharpPointWriter, self).__init__(*args, **kwargs)
+    def start(self):
+        pass
+    def writedict(self, dct, level=0, recurse=False):
+        pass
+    def writeiterable(self, iterable, func=None, counter=''):
+        if self.p.csv_counter:
+            iterable = itertools.chain([counter], iterable)
+
+        if func is not None:
+            iterable = map(lambda x: func(x), iterable)
+        items = list(iterable)[3:9]
+        items[0] = items[0].replace(" ", "/")
+        items[0] = items[0].replace(":", "/").replace("-", "/")
+        line = self.p.csvsep.join(items)
+        self.writeline(line)
